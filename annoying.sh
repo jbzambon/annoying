5e7a4fbc413d0f2550052522a0359ab2287aa17b
Joseph B. Zambon <jbzambon@ncsu.edu>
2023-05-31 12:46:57 -0400

    Create annoying.sh

---
+++ annoying.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+
+handy=`./handy.sh`
+
+#echo $handy
+
+echo "\n\nHello [Name of person you hate].  Congrats on the wedding, the current UTC time is `date` \n\nYour randomly generated Jack Handy quote is... \n\n$ha
+ndy \n\n<3 Joe" | mail -s "" [Email of person you hate]
